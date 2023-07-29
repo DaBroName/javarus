9b589abdf26d59da194c28827e91425dc2adf984
Pavel Tsukanov <pavel.tsukanov@gmail.com>
2023-07-30 01:15:33 +0300

    Создание проекта с решением задач и добавление задач task01, task02

---
+++ src/task01/task02.java
@@ -0,0 +1,15 @@
+/*
+Написать программу, выводящую на экран надпись "Элли самая красивая" 5 раз.
+Каждый раз - с новой строки.
+ */
+package task01;
+
+public class task02 {
+    public static void main(String[] args) {
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+    }
+}
