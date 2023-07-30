6da353b37a133f037cdf9393a25f82132cf5e8f5
Pavel Tsukanov <pavel.tsukanov@gmail.com>
2023-07-30 20:48:21 +0300

    Решены задачи task06, task07

---
+++ src/task01/task06.java
@@ -0,0 +1,12 @@
+/*
+Напиши программу, которая выводит на экран надпись:
+Это не баг, это фича.
+ */
+
+package task01;
+
+public class task06 {
+    public static void main(String[] args) {
+        System.out.println("Это не баг, это фича.");
+    }
+}
