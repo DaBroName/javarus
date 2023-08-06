f6acfb33801f20ab969310ebd8f586569ca6ab47
Pavel Tsukanov <pavel.tsukanov@gmail.com>
2023-08-06 22:45:09 +0300

    Решены две задачи task14 и task15

---
+++ src/task01/task15.java
@@ -0,0 +1,25 @@
+/*
+Убери комментарии с части кода, чтобы на экран вывелось сообщение "Happy New Year".
+
+Требования:
+•	Программа должна выводить на экран текст.
+•	Нужно раскомментировать некоторые строки. Больше ничего не менять.
+•	В программу нельзя добавлять новые строки или удалять имеющиеся.
+•	Выведенный текст должен соответствовать заданию.
+ */
+package task01;
+
+public class task15 {
+    public static void main(String[] args) {
+        // String s = "Merry";
+        // System.out.println("Christmas");
+        // System.out.println("Merry New");
+        // System.out.println("weekend");
+        System.out.print("Happy New");
+        // System.out.println("weekend");
+        // System.out.println(s);
+        // System.out.print("Merry New!");
+        System.out.print(" ");
+        System.out.println("Year");
+    }
+}
