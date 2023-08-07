24f4aea7ccbd24af5d8aaa69fe40b15a9dede01a
Pavel Tsukanov <pavel.tsukanov@gmail.com>
2023-08-07 20:56:55 +0300

    Решены задачи из task2. Задачи task01, 02, 02

---
+++ src/task02/task02.java
@@ -0,0 +1,20 @@
+/*
+В переменной centimeters записано количество сантиметров.
+В переменную meters нужно записать количество полных метров в centimeters (1 метр = 100 см).
+Для вычисления используй переменную centimeters и оператор деления.
+Для объявления и инициализации meters используй одну команду.
+
+Требования:
+•	Не изменяй значение переменной centimeters.
+•	Для вычисления meters должны использоваться centimeters и оператор деления.
+•	Программа должна выводить на экран значение переменной meters.
+ */
+package task02;
+
+public class task02 {
+    public static void main(String[] args) {
+        int centimeters = 243;
+        int meters = centimeters / 100; //напишите тут ваш код
+        System.out.println(meters);
+    }
+}
