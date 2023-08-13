023d1e49de795e0b2a25283823180267a041e92a
Pavel Tsukanov <pavel.tsukanov@gmail.com>
2023-08-13 23:30:59 +0300

    + задачи task04, 05, 06

---
+++ src/task02/task05.java
@@ -0,0 +1,24 @@
+/*
+Используя только оператор декремента, измени значение переменной toothCounter, чтобы на экран вывелось число 23.
+Требования:
+•	Не изменяй команду объявления переменной toothCounter.
+•	К переменной toothCounter нужно применить несколько команд декремента, чтобы она изменила значение на 23.
+•	Программа должна выводить на экран переменную toothCounter со значением 23.
+ */
+package task02;
+
+public class task05 {
+    public static void main(String[] args) {
+        int toothCounter = 32;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        System.out.println(toothCounter);
+    }
+}
---
+++ src/task02/task06.java
@@ -0,0 +1,18 @@
+/*
+В методе main объяви переменные word, phrase, line и text типа String.
+Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+Значениями могут быть любые строки.
+
+Требования:
+•	В методе main объяви четыре переменные типа String.
+•	Назови переменные именами word, phrase, line и text.
+•	Переменным сразу присвой значения.
+•	У всех переменных должны быть разные значения.
+ */
+package task02;
+
+public class task06 {
+    public static void main(String[] args) {
+        String word = "slovo", text = "tekst", line = "liniy", phrase = "fraza";
+    }
+}
