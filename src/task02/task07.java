3accb4f2692a4f4892176e46f6cdb645c298da6a
Pavel Tsukanov <pavel.tsukanov@gmail.com>
2023-08-16 23:58:31 +0300

    Решены 3 задачи: 07 - 09

---
+++ src/task02/task07.java
@@ -0,0 +1,23 @@
+/*
+В переменную tagline нужно записать строку "JustDoIt".
+Используй s1, s2, s3 и конкатенацию строк.
+
+Требования:
+•	Не изменяй значения переменных s1, s2, s3.
+•	Для инициализации переменной tagline используй s1, s2, s3 и конкатенацию строк.
+•	Программа должна выводить на экран значение переменной tagline.
+ */
+
+package task02;
+
+public class task07 {
+    public static void main(String[] args) {
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String tagline = s3 + s1 + s2; //напишите тут ваш код
+
+        System.out.println(tagline);
+    }
+}
